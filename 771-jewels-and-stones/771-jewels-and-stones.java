000121657872e8e98225c8628ade985d5fa00aa0
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-08-21 23:46:58 +0530

    Time: 3 ms (33.60%), Space: 42.2 MB (63.71%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+
+    class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c = 0;
+        for(int i = 0; i < jewels.length(); i++){
+            for(int j = 0; j < stones.length(); j++){
+                if(stones.charAt(j) == jewels.charAt(i))
+                    c++;
+            }
+        }
+        return c;
+    }
+}
