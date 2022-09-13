843f175bbba9a7f1d8c4c2598909d2f5842099f1
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-09-13 23:37:16 +0530

    Time: 162 ms (14.60%), Space: 86.5 MB (17.81%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,37 @@
+class ListNode {
+    String page;
+    ListNode next;
+    ListNode prev;
+    ListNode(String page) { this.page = page; }
+}
+
+
+class BrowserHistory {
+
+    ListNode head;
+    
+    public BrowserHistory(String homepage) {
+        head = new ListNode(homepage);
+        head.prev = null;
+    }
+    
+    public void visit(String url) {
+        ListNode prev = head;
+        head.next = new ListNode(url);
+        head = head.next;
+        head.prev = prev;
+    }
+    
+    public String back(int steps) {
+        while(steps-- > 0 && head.prev != null) head = head.prev;
+        
+        return head.page;
+    }
+    
+    public String forward(int steps) {
+        while(steps-- > 0 && head.next != null) head = head.next;
+        while(true)
+        
+        return head.page;
+    }
+}
