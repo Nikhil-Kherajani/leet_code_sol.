620818f3f2ff8d51da1ce7f4e0591ea17ded00d4
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-10-15 23:53:46 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (99.91%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2!=0){
+            return n*2 ;
+        }
+        else{
+            return n;
+        }
+        
+    }
+}
