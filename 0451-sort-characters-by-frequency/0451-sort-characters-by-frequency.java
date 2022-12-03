2b4ae7ebcb79b03ca7ef1fa6db63ed3a0dbc64f2
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-12-03 23:55:57 +0530

    Time: 11 ms (96.95%), Space: 43.1 MB (92.52%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,31 +1,42 @@
 class Solution {
     public String frequencySort(String s) {
-		Map<Character,Integer> map=new HashMap<Character, Integer>();
-		Queue<Map.Entry<Character,Integer>> qu=new PriorityQueue<>(
-				(a,b)->{
-					if(a.getValue() != b.getValue())
-						return -a.getValue().compareTo(b.getValue());
-					return a.getKey().compareTo(b.getKey());
-				}
-				);
-		for(int i=0;i<s.length();i++) {
-			if(map.containsKey(s.charAt(i))) map.put(s.charAt(i),map.get(s.charAt(i))+1);
-			else map.put(s.charAt(i),1);
-		}
-		
-		for(Map.Entry<Character,Integer> m:map.entrySet()) {
-			qu.offer(m);
-		}
-		String str="";
-		while(!qu.isEmpty()) {
-			int n=qu.peek().getValue();
-			char ch=qu.poll().getKey();
-			while(n>0) {
-				str=str+ch;
-				n--;
-			}
+        // PriorityQueue for sorting elements on decresing order based on frequency
+        // b.val - a.val => store pair objects based on decresong order of frequency value 
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.val-a.val);
+        // HashMap to store elements and their current frequency
+        HashMap<Character,Integer> hm = new HashMap<>();
+        char []c = s.toCharArray();
+        for(int i=0;i<c.length;i++) {
+            int count = hm.getOrDefault(c[i],0);
+            //add element and current frequency in hashmap
+            hm.put(c[i],count+1);
         }
+        //add whole hashmap to priorityQueue
+        for(Map.Entry<Character,Integer> entry: hm.entrySet()) {
+            Pair pair = new Pair(entry.getKey(),entry.getValue());
+            pq.add(pair);
+        }
+        StringBuilder sb = new StringBuilder("");
+        //extract value from priorityQueue and add it into answer string
+        while(!pq.isEmpty()) {
+            Pair pair = pq.remove();
+            int count = pair.val;
+            char ch = pair.c;
+            // add character into answer string as many time as frequency (it appears in main string)
+            while(count--> 0) {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
     
-		return str;
+    // create class to store character and their respective frequency
+    public class Pair {
+        int val;
+        char c;
+        public Pair(char c,int val) {
+            this.c = c;
+            this.val = val;
+        }
     }
 }
