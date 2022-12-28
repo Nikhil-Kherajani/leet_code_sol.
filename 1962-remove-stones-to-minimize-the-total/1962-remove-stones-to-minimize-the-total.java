8b0d4e2bbbcc29d2791d186e0e71cd91e4ac53d4
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2022-12-28 13:52:00 +0530

    Time: 663 ms (70.65%), Space: 57.6 MB (76.30%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -17,18 +17,18 @@ class Solution {
         for (int i = 0; i < piles.length; i++) {
             pq.add(piles[i]);
         }
-        // System.out.println(pq.size());
+       
         double two = 2;
 
         while (pq.size() > 0 && k > 0) {
-            // System.out.println((int) Math.ceil(pq.remove() / two));
+            
             pq.add((int) Math.ceil(pq.remove() / two));
 
             k--;
         }
         int ans = 0;
         while (pq.size() > 0) {
-            System.out.println(pq.peek());
+            
             ans += pq.remove();
         }
 
