b62c5a0ef9aa1f79aea3224401f3f01b6af25c7d
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-01-02 16:54:23 +0530

    Time: 27 ms (6.36%), Space: 42.4 MB (96.55%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int closetTarget(String[] words, String target, int startIndex) {
+        String[] newa = new String[words.length * 3];
+        if (words.length == 1) {
+            if (words[0].equals(target))
+                return 0;
+            else
+                return -1;
+        }
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < words.length; j++) {
+                newa[k] = words[j];
+                k++;
+            }
+        }
+        int rcount = 0;
+        boolean rflag = false, lflag = false;
+        for (int i = startIndex + words.length; i < newa.length; i++) {
+
+            System.out.println(newa[i]);
+            if (newa[i].equals(target)) {
+                System.out.println(newa[i]);
+                rflag = true;
+                break;
+            }
+            rcount++;
+        }
+        int lcount = 0;
+        for (int i = startIndex + words.length; i >= 0; i--) {
+            if (newa[i].equals(target)) {
+                lflag = true;
+                break;
+            }
+            lcount++;
+        }
+        if (rflag && lflag) {
+
+            return Math.min(rcount, lcount);
+        } else if (rflag && !lflag) {
+            return rcount;
+        } else if (!rflag && lflag) {
+            return lcount;
+        } else {
+            return -1;
+        }
+
+    }
+}
