59b83b8248f2e78c51f0f12ef3e97f01401196c5
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-01-04 03:37:27 +0530

    Time: 4 ms (27.55%), Space: 45.7 MB (42.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,10 +11,10 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        while (head != null) {
-            if (head.val == 1_000_000) return true;
-            head.val = 1_000_000;
-            head = head.next;
+        while (head!=null){
+            if (head.val==-999999) return true;
+            head.val=-999999;
+            head=head.next;
         }
         return false;
     }
