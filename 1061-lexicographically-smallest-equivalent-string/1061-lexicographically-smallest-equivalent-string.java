33ac2d9ee717e6c2078bb2ba9eb78e1a6bb9e3b8
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-01-14 17:52:11 +0530

    Time: 1 ms (100.00%), Space: 40 MB (100.00%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,36 +1,32 @@
 class Solution {
-    int[] root = new int[26];
+    private int[] p;
 
-    // recursively get the root element
-    int get(int x) {
-        return x == root[x] ? x : (root[x] = get(root[x]));
-    }
-
-    // unite two elements
-    void unite(int x, int y) {
-        // find the root of x
-        x = get(x);
-        // find the root of y
-        y = get(y);
-        // if their roots are not same, we combine them
-        if (x != y) {
-            // smaller first
-            if (x < y)  root[y] = x;
-            else root[x] = y;
-        }
-        return;
-    }
-    
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        String ans = "";
-        // init root. initialy each element is in its own group.
-        for (int i = 0; i < 26; i++)  root[i] = i;
-        // unite each character
-        for (int i = 0; i < s1.length(); i++) unite(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
-        // build the final answer from the root element (smallest)
-        for (int i = 0; i < baseStr.length(); i++) {
-            ans += (char)(get(baseStr.charAt(i) - 'a') + 'a');
+        p = new int[26];
+        for (int i = 0; i < 26; ++i) {
+            p[i] = i;
         }
-        return ans;
+        for (int i = 0; i < s1.length(); ++i) {
+            int a = s1.charAt(i) - 'a', b = s2.charAt(i) - 'a';
+            int pa = find(a), pb = find(b);
+            if (pa < pb) {
+                p[pb] = pa;
+            } else {
+                p[pa] = pb;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char a : baseStr.toCharArray()) {
+            char b = (char) (find(a - 'a') + 'a');
+            sb.append(b);
+        }
+        return sb.toString();
+    }
+
+    private int find(int x) {
+        if (p[x] != x) {
+            p[x] = find(p[x]);
+        }
+        return p[x];
     }
 }
