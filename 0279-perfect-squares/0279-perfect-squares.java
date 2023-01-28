c2492d382353afc5b1a8c706904f98a991ef5b9a
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-01-28 18:26:52 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (82.90%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,24 +1,24 @@
-class Solution {
-    public int numSquares(int n) {
-        int [] squares = new int[(int)Math.sqrt(n)];
-        for(int i=0;i<squares.length;i++){
-            squares[i]=(i+1)*(i+1);
-        }
-        int [][] dp = new int [(int)Math.sqrt(n)][n+1];
-        for(int i=0;i<n+1;i++){
-            dp[0][i]=i;
-        }
-        for(int i=1;i<(int)Math.sqrt(n);i++){
-            for(int j=0;j<n+1;j++){
-                if(j-squares[i]>=0){
-                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-squares[i]]+1);
-                }
-                else{
-                    dp[i][j]= dp[i-1][j];
-                }
-            }
-        }
-        return dp[(int)Math.sqrt(n)-1][n];
-    }
+class Solution{
+public int numSquares(int n) {
+	int sqrt = (int) Math.sqrt(n);
 
+	if (sqrt * sqrt == n) // Perfect square
+		return 1;
+
+	while (n % 4 == 0) // 4^a (8b + 7)
+		n = n / 4;
+
+	if (n % 8 == 7)
+		return 4;
+
+	for (int i = 1; i * i <= n; i++) { // Sum of two perfect squares
+		int square = i * i;
+		int base = (int) Math.sqrt(n - square);
+
+		if (base * base == n - square)
+			return 2;
+	}
+
+	return 3;
+}
 }
