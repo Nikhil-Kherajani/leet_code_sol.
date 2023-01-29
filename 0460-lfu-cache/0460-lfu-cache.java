aef7521b2df4b6dbb30d281c9ae2a407a6d1a792
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-01-29 22:35:12 +0530

    Time: 61 ms (94.68%), Space: 117.3 MB (98.94%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,55 +1,149 @@
 class LFUCache {
-  public LFUCache(int capacity) {
-    this.capacity = capacity;
-  }
-
-  public int get(int key) {
-    if (!keyToVal.containsKey(key))
-      return -1;
-
-    final int freq = keyToFreq.get(key);
-    freqToLRUKeys.get(freq).remove(key);
-    if (freq == minFreq && freqToLRUKeys.get(freq).isEmpty()) {
-      freqToLRUKeys.remove(freq);
-      ++minFreq;
+   int capacity;
+    HashMap<Integer, Node> keyNodeMap = new HashMap<>();
+    HashMap<Integer, NodeDLinkedList> freqNodeDLLMap = new HashMap<>();
+    int minumumFrequency = 1;
+    
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
     }
-
-    // Increase key's freq by 1
-    // Add this key to next freq's list
-    putFreq(key, freq + 1);
-    return keyToVal.get(key);
-  }
-
-  public void put(int key, int value) {
-    if (capacity == 0)
-      return;
-    if (keyToVal.containsKey(key)) {
-      keyToVal.put(key, value);
-      get(key); // Update key's count
-      return;
+    
+    public int get(int key) {
+        Node node = keyNodeMap.get(key);
+        if(node != null){                                                              
+            incrementFrequency(node);                                                  
+            return node.value;                                                         
+        }
+        else{
+            return -1;
+        }
     }
-
-    if (keyToVal.size() == capacity) {
-      // Evict LRU key from the minFreq list
-      final int keyToEvict = freqToLRUKeys.get(minFreq).iterator().next();
-      freqToLRUKeys.get(minFreq).remove(keyToEvict);
-      keyToVal.remove(keyToEvict);
+    
+    public void put(int key, int value) {
+        
+        if(capacity <= 0) return;
+        
+        if(keyNodeMap.containsKey(key)){                                                     
+            Node node = keyNodeMap.get(key);                                            
+            node.value = value;                                                        
+            incrementFrequency(node);                                                
+            keyNodeMap.put(key, node);                                                 
+        }
+        else{                                                                        
+            
+            Node node = new Node(key, value);                                    
+            
+            if(keyNodeMap.size() == capacity){                                          
+                Node removedLastNode = freqNodeDLLMap.get(minumumFrequency)
+                                        .removeLastNode();                          
+                keyNodeMap.remove(removedLastNode.key);                   
+            }
+            
+            incrementFrequency(node);                               
+            keyNodeMap.put(key, node);                      
+            
+            minumumFrequency = 1;                                                      
+                                                    
+        }
+        
     }
+    
+    
+    private void incrementFrequency(Node node){
+        
+        int oldFrequency = node.frequency;
+        
+        if(freqNodeDLLMap.containsKey(oldFrequency)){                    
+           NodeDLinkedList oldNodeDLinkedList = freqNodeDLLMap.get(oldFrequency);      
+            oldNodeDLinkedList.remove(node);           
+            if(node.frequency == minumumFrequency &&                                    
+                oldNodeDLinkedList.length == 0){                                        
+                minumumFrequency++;                                         
+            }
+        }
 
-    minFreq = 1;
-    putFreq(key, minFreq);    // Add new key and freq
-    keyToVal.put(key, value); // Add new key and value
-  }
+        int newFrequency = oldFrequency + 1;                         
+        node.frequency = newFrequency;                                     
+        NodeDLinkedList newNodeDLinkedList =                                       
+            freqNodeDLLMap.getOrDefault(                            
+            newFrequency, new NodeDLinkedList()
+        );           
+        newNodeDLinkedList.add(node);                                            
+        freqNodeDLLMap.put(newFrequency, newNodeDLinkedList);                          
+    }
+    
+    
+    private class Node{
+        int key;
+        int value;
+        int frequency;
+        Node prev;
+        Node next;
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+            this.frequency = frequency;
+        }
+    }
+    
+    private class NodeDLinkedList{
+        
+        Node head, tail;
+        int length;
+        
+        //Add a node to top
+        void add(Node node){
+            
+            //Remove old pointers
+            node.prev = null;
+            node.next = null;
+                
+            if(head==null){                                   
+                head = node;
+                tail = node;
+            }
+            else{
+                node.next = head;                                  
+                head.prev = node;                                   
+                head = node;
+            }
+            
+            length++;
+        }
+        
+        //Remove a node
+        void remove(Node node){
+            
+            if(node==head){                                                 
+                if(node==tail){                                                           
+                    tail=null;                                    
+                }
+                head = head.next;                                                   
+            }
+            else{                                                            
+                node.prev.next = node.next;                        
 
-  private int capacity;
-  private int minFreq = 0;
-  private Map<Integer, Integer> keyToVal = new HashMap<>();
-  private Map<Integer, Integer> keyToFreq = new HashMap<>();
-  private Map<Integer, LinkedHashSet<Integer>> freqToLRUKeys = new HashMap<>();
-
-  private void putFreq(int key, int freq) {
-    keyToFreq.put(key, freq);
-    freqToLRUKeys.putIfAbsent(freq, new LinkedHashSet<>());
-    freqToLRUKeys.get(freq).add(key);
-  }
+                if(node==tail){                                             
+                    tail = node.prev;                                      
+                }
+                else{
+                    node.next.prev = node.prev;                     
+                }
+            }
+            
+            length--;
+            
+        }
+        
+        //Remove last node
+        Node removeLastNode(){
+            
+            Node tailNode = tail;
+            
+            if(tailNode != null){                                   
+                remove(tailNode);
+            }
+            return tailNode;
+        }
+    }
 }
