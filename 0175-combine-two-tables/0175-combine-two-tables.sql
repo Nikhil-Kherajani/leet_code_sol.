f88f89791e1e9019d26a7b19c111e0cd506b8342
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-02-08 23:38:01 +0530

    Time: 908 ms (14.43%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,1 +1,1 @@
-SELECT FirstName, LastName, City, Address.State from Person LEFT JOIN Address on Person.PersonId = Address.PersonId;
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State from Person LEFT JOIN Address on Person.PersonId = Address.PersonId;
