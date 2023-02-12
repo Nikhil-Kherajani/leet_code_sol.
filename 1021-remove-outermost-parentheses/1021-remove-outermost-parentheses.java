5ba8ee1ecc0df8569eb6f3103f2ed6ccf1c67354
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-02-13 02:49:00 +0530

    Time: 5 ms (65.09%), Space: 42.6 MB (39.62%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,26 +1,22 @@
+import java.util.*;
+
 class Solution {
     public String removeOuterParentheses(String s) {
-        //Get the start and end indices, 
-        //where the level start to 1 and reduces to zero
-        //then remvoe those indices.
-        
-        final StringBuilder sb = new StringBuilder();
-        int len = s.length();
-       
-        int level = 0;
-        
-        for(int i = 0; i < len; ++i){
-            char ch = s.charAt(i);
+        int count = 0;
+        int start = 1;
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                ans += s.substring(start , i);
+                start = i + 2;
+            }
             
-            if(ch == '(') ++level;
-            
-            if(level > 1) sb.append(ch);
-            
-            if(ch == ')') --level;
-            
-                
         }
-        
-        return sb.toString();
+        return ans;
     }
 }
