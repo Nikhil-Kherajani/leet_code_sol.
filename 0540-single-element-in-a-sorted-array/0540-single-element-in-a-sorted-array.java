d8c5e85e04601c036d6584f555eb8ed8814d8c91
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-02-21 13:32:39 +0530

    Time: 1 ms (30.40%), Space: 48 MB (85.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,19 +1,11 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==2) return nums[0];
-        int l = 0;
-        int h = nums.length-1;
         
-        while(l<h){
-            int mid = l+(h-l)/2;      // divide the array
-            
-            if(nums[mid]==nums[mid+1]) mid = mid-1;      //two same elements should be in same half
-            
-            if((mid-l+1)%2!=0) h = mid;            // checking the length of left half. If its is odd then update ur right pointer to mid
-            
-            else l = mid+1;    // else your right half will be odd then update your left pointer to mid+1
+        final int n = nums.length; 
+        int xor = 0;
+        for(int i = 0 ; i < n ; i++){
+            xor ^= nums[i];
         }
-        
-        return nums[l];     //left pointer will have the answer at last
+        return xor;
     }
 }
