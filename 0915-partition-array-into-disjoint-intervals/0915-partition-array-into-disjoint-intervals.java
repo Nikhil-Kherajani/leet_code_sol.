ebab712502b5fe9eea307bbd08032d7d3f95a7b9
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-03-17 01:16:57 +0530

    Time: 3 ms (73.40%), Space: 51.3 MB (93.27%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
@@ -1,18 +1,17 @@
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int count = 0, last = 1 , ans = 0;
+        int count = 0, last = 1 , ans = 0 , j = 1;
         final int n = nums.length;
     
         for(int i = 1 ; i < n ; i++){
             if(nums[i] < nums[count]){
                 ans = i;
-                for(int j = last ; j < i; j++){
+                for(j = last ; j < i; j++){
                     if(nums[j] > nums[count]){
                         count = j;
-                        
                     }
-                    last = j;
                 }
+                last = j;
             }
         }
         return ans + 1;
