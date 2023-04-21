042d9997869bd9397a266d063ea6145ff2a19c0f
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-04-22 00:01:32 +0530

    Time: 135 ms (19.18%), Space: 53.2 MB (20.55%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,31 +1,36 @@
 class Solution {
-    int mod=1000000007;
-    public int trav(int n,int p,int group[],int profit[],int index,int dp[][][])
-    {
-        int ans=0;
-        if(n<0)
-            return ans;
-        if(index>=group.length)
-        {
-            if(p<=0)
-                return 1;
-            return ans;
+    int mod = 1000000007;
+    int[][][] memo = new int[101][101][101];
+    
+    int find(int pos, int count, int profit, int n, int minProfit, int[] group, int[] profits) {
+        if (pos == group.length) {
+            // If profit exceeds the minimum required; it's a profitable scheme.
+            return profit >= minProfit ? 1 : 0;
         }
-        if(p<0)
-            p=0;
-        if(dp[index][n][p]!=-1)
-            return dp[index][n][p];
-        ans=(ans%mod+trav(n-group[index],p-profit[index],group,profit,index+1,dp)%mod)%mod;
-        ans=(ans%mod+trav(n,p,group,profit,index+1,dp)%mod)%mod;
-        dp[index][n][p]=ans;
-        return ans;
+        
+        if (memo[pos][count][profit] != -1) {
+            // Repeated subproblem, return the stored answer.
+            return memo[pos][count][profit];
+        }
+        
+        // Ways to get a profitable scheme without this crime.
+        int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
+        if (count + group[pos] <= n) {
+            // Adding ways to get profitable schemes, including this crime.
+            totalWays += find(pos + 1, count + group[pos], Math.min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
+        }
+        
+        return memo[pos][count][profit] = totalWays % mod;
     }
+    
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-        int dp[][][]=new int[profit.length][n+1][minProfit+1];
-        for(int i=0;i<profit.length;i++)
-            for(int j=0;j<=n;j++)
-                for(int k=0;k<=minProfit;k++)
-                    dp[i][j][k]=-1;
-        return trav(n,minProfit,group,profit,0,dp);
+        // Initializing all states as -1.
+        for (int i = 0; i <= group.length; i++) {
+            for(int j = 0; j <= n; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+        
+        return find(0, 0, 0, n, minProfit, group, profit);
     }
 }
