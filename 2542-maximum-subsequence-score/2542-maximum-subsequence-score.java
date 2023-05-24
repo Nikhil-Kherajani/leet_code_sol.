6b7a26f9048d4b94ef04da3984e2037a0ccd55c2
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-05-24 23:25:31 +0530

    Time: 105 ms (40.23%), Space: 60.3 MB (48.85%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -1,56 +1,33 @@
-import java.util.*;
-
 class Solution {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        int[][] num = new int[nums1.length][2];
-        for (int i = 0; i < nums1.length; i++) {
-            num[i][0] = nums1[i];
-            num[i][1] = nums2[i];
+
+        int n = nums1.length;
+        int[][] pairs = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            pairs[i] = new int[] {nums1[i], nums2[i]};
+        }
+        
+        Arrays.sort(pairs, (n1, n2) -> n2[1] - n1[1]);
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
+        long ksum = 0;
+
+        for (int i = 0; i < k; i++) {
+            ksum += pairs[i][0];
+            heap.add(pairs[i][0]);
         }
 
-        Arrays.sort(num,
-                (a, b) -> Integer.compare(a[1], b[1]) == 0 ? Integer.compare(b[0], a[0]) : Integer.compare(a[1], b[1]));
-        int l = 0;
-        int r = num.length - 1;
-        while (l < r) {
-            int temp0 = num[l][0];
-            int temp1 = num[l][1];
-            num[l][0] = num[r][0];
-            num[l][1] = num[r][1];
-            num[r][0] = temp0;
-            num[r][1] = temp1;
-            l++;
-            r--;
-        }
-        // for (int i = 0; i < num.length; i++) {
-        //     System.out.println(num[i][0] + " " + num[i][1]);
-        // }
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        long sum = 0;
-        long maxSum = -1;
-        for (int i = 0; i < k - 1; i++) {
-            pq.add(num[i][0]);
-            sum += num[i][0];
+        long ans = ksum * pairs[k - 1][1];
+
+        for (int i = k; i < n; i++) {
+            ksum += pairs[i][0];
+            ksum -= heap.poll();
+            heap.add(pairs[i][0]);
+
+            ans = Math.max(ans, ksum * pairs[i][1]);
         }
 
-        for (int i = k - 1; i < num.length; i++) {
-            // System.out.println(i + " " + pq.size());
-            if (pq.size() < k) {
-                pq.add(num[i][0]);
-                sum += num[i][0];
-            } else {
-                if (num[i][0] > pq.peek()) {
-                    sum -= pq.poll();
-                    pq.add(num[i][0]);
-                    sum += num[i][0];
-                }
-
-            }
-
-            maxSum = Math.max(maxSum, sum * num[i][1]);
-
-        }
-        return maxSum;
-
+        return ans;
     }
 }
