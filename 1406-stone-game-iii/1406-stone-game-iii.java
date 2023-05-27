efb4320204f0d8784ab3b9a49ff430b2d81e51f2
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-05-27 19:19:10 +0530

    Time: 92 ms (14.57%), Space: 64.8 MB (5.03%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -6,7 +6,6 @@ class Solution {
         
         cache = new Integer[n + 1];
         
-        //AliceScore = Stone Collected by Alice - Stone Collected by Bob
         int AliceScore = getAliceScore(piles, 0);
         
         if(AliceScore > 0) return "Alice";
@@ -19,7 +18,6 @@ class Solution {
     private int getAliceScore(int[] piles, int index){
         if(index == piles.length) return 0;
         
-        //reterive from cache
         if(cache[index] != null) return cache[index];
         
         int maxScore = Integer.MIN_VALUE;
@@ -29,19 +27,15 @@ class Solution {
         for(int x = 0; x < 3; x++){
             int i = index + x;
             
-            //ignore the invalid index
             if(i >= piles.length) continue;
             
             stone += piles[i];
 
-            //calculate score
             int score = stone - getAliceScore(piles, i + 1);
             
-            //update score
             maxScore = Math.max(maxScore, score);
         }
         
-        //save in cache
         return cache[index] = maxScore;
     }
     
