969472d006ded13c91461d91c2ecd97c96c5107f
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2023-05-30 13:38:02 +0530

    Time: 17 ms (66.08%), Space: 48.6 MB (92.63%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,5 +1,21 @@
-class MyHashSet extends HashSet<Integer> {
-
+class MyHashSet  {
+int size = (int)Math.pow(10, 6)+1;
+	boolean[] flag;
+    public MyHashSet() {
+		flag = new boolean[size];
+	}
+    
+    public void add(int key) {
+        flag[key]=true;
+    }
+    
+    public void remove(int key) {
+        flag[key]=false;
+    }
+    
+    public boolean contains(int key) {
+        return flag[key];
+    }
 }
 
 /**
