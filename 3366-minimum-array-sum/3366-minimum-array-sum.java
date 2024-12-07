3a5622535d6578e7bfb502cbde3c7fbe20913680
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2024-12-07 12:27:31 +0530

    Time: 63 ms (51.82%), Space: 56.8 MB (15.15%) - LeetHub

--- 3366-minimum-array-sum/3366-minimum-array-sum.java
+++ 3366-minimum-array-sum/3366-minimum-array-sum.java
@@ -1,41 +1,60 @@
+import java.util.Arrays;
+
 class Solution {
     public int minArraySum(int[] nums, int k, int op1, int op2) {
-        int[][][] dp = new int[nums.length+1][op1+1][op2+1];
-        for(int[][] ele2: dp){
-            for(int[] ele1: ele2){
-                Arrays.fill(ele1,-1);
+        int n = nums.length;
+        long[][][] dp = new long[n + 1][op1 + 1][op2 + 1];
+        for (long[][] row : dp) {
+            for (long[] col : row) {
+                Arrays.fill(col, -1);
             }
         }
-        return solve(nums,0,op1,op2,k,dp);
+
+        return (int) solve(nums, k, op1, op2, 0, dp);
     }
-    public int solve(int[] nums, int i,int op1, int op2, int k, int[][][] dp){
-        if(i >= nums.length){
+
+    private long solve(int[] nums, int k, int op1, int op2, int index, long[][][] dp) {
+        if (index == nums.length) {
             return 0;
         }
-        if(dp[i][op1][op2] != -1){
-            return dp[i][op1][op2];
+
+        if (dp[index][op1][op2] != -1) {
+            return dp[index][op1][op2];
         }
-        int operation1 = Integer.MAX_VALUE;
-        if(op1 > 0){
-            int temp =  (int)Math.ceil(nums[i]/(2*1.0));
-            int val1 = temp+solve(nums,i+1,op1-1,op2,k,dp);
-            int val2 = Integer.MAX_VALUE;
-            if(temp >= k && op2 > 0){
-                val2 = (temp-k) + solve(nums,i+1,op1-1,op2-1,k,dp);
+
+        // Option 1: Skip both operations
+        long ans = nums[index] + solve(nums, k, op1, op2, index + 1, dp);
+
+        // Option 2: Apply only operation 1
+        if (op1 > 0) {
+            ans = Math.min(ans, (nums[index] + 1) / 2 + solve(nums, k, op1 - 1, op2, index + 1, dp));
+        }
+
+        // Option 3: Apply only operation 2
+        if (op2 > 0 && nums[index] >= k) {
+            ans = Math.min(ans, nums[index] - k + solve(nums, k, op1, op2 - 1, index + 1, dp));
+        }
+
+        // Option 4: Apply operation 1 then operation 2
+        if (op1 > 0 && op2 > 0) {
+            int nextVal = (nums[index] + 1) / 2;
+            if (nextVal >= k) {
+                ans = Math.min(ans, nextVal - k + solve(nums, k, op1 - 1, op2 - 1, index + 1, dp));
+            } else if (op1 > 1){
+                ans = Math.min(ans, Math.max(0, nextVal) + solve(nums, k, op1 - 1, op2, index+1, dp) );
+
             }
-            operation1 = Math.min(val1,val2);
-        }
-        int operation2 = Integer.MAX_VALUE;
-        if(op2 > 0 && nums[i] >= k){
-            int temp = nums[i]-k;
-            int val1 = temp + solve(nums,i+1,op1,op2-1,k,dp);
-            int val2 = Integer.MAX_VALUE;
-            if(op1 > 0){
-                val2 = (int) Math.ceil(temp/(2*1.0)) + solve(nums,i+1,op1-1,op2-1,k,dp);
+             else {
+                ans = Math.min(ans, nextVal + solve(nums,k,op1-1,op2,index+1,dp));
             }
-            operation2 = Math.min(val1,val2);
         }
-        int skip = nums[i] + solve(nums,i+1,op1,op2,k,dp);
-        return dp[i][op1][op2] = Math.min(skip,Math.min(operation1,operation2));
+
+        // Option 5: Apply operation 2 then operation 1
+        if (op1 > 0 && op2 > 0 && nums[index] >= k) {
+            ans = Math.min(ans, (nums[index]-k+1)/2 + solve(nums, k, op1-1, op2-1, index+1, dp));
+        }
+        
+
+        return dp[index][op1][op2] = ans;
     }
 }
