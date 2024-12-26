6747dc1e176096f5f044abc0418246bfd04c5cae
Nikhil Kherajani <nikhilkherajani7@gmail.com>
2024-12-26 17:29:03 +0530

    Time: 54 ms (66.18%), Space: 56.8 MB (82.88%) - LeetHub

--- 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.java
+++ 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.java
@@ -2,33 +2,31 @@ class Solution {
     public int countPathsWithXorValue(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int MOD = 1000000007;
-        
-        // Initialize the DP table
+        int mod = 1000000007;
+
+        // dp[i][j][xor] stores the number of paths from (0, 0) to (i, j) with xor value equal to xor
         int[][][] dp = new int[m][n][16];
+
+        // Base case: There is 1 path from (0, 0) to (0, 0) with xor value equal to grid[0][0]
         dp[0][0][grid[0][0]] = 1;
-        
-        // Iterate through each cell
+
+        // Iterate over the grid and fill the dp array
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                for (int x = 0; x < 16; x++) {
-                    if (dp[i][j][x] > 0) {
-                        // Move right
-                        if (j + 1 < n) {
-                            int newX = x ^ grid[i][j + 1];
-                            dp[i][j + 1][newX] = (dp[i][j + 1][newX] + dp[i][j][x]) % MOD;
-                        }
-                        // Move down
-                        if (i + 1 < m) {
-                            int newX = x ^ grid[i + 1][j];
-                            dp[i + 1][j][newX] = (dp[i + 1][j][newX] + dp[i][j][x]) % MOD;
-                        }
+                for (int xor = 0; xor < 16; xor++) {
+                    // If we can reach (i, j) from (i - 1, j)
+                    if (i > 0) {
+                        dp[i][j][xor ^ grid[i][j]] = (dp[i][j][xor ^ grid[i][j]] + dp[i - 1][j][xor]) % mod;
+                    }
+                    // If we can reach (i, j) from (i, j - 1)
+                    if (j > 0) {
+                        dp[i][j][xor ^ grid[i][j]] = (dp[i][j][xor ^ grid[i][j]] + dp[i][j - 1][xor]) % mod;
                     }
                 }
             }
         }
-        
-        // The result is the number of ways to reach (m-1, n-1) with XOR value k
+
+        // Return the number of paths from (0, 0) to (m - 1, n - 1) with xor value equal to k
         return dp[m - 1][n - 1][k];
     }
 }
